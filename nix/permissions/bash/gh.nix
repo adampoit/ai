d797c93472ffc2f71dff75937ebcb7891b9dbf55
Adam Poit <metalkeys404@gmail.com>
2026-04-16 11:43:47 -0700

    Set most gh actions back to "allow" and explicitly define "ask".

--- nix/permissions/bash/gh.nix
+++ nix/permissions/bash/gh.nix
@@ -1,62 +1,99 @@
-{
-  "gh *" = "ask";
-
-  "gh api" = "allow";
-  "gh api *" = "allow";
-
-  "gh api -X GET*" = "allow";
-  "gh api -XGET*" = "allow";
-  "gh api * -X GET*" = "allow";
-  "gh api * -XGET*" = "allow";
-  "gh api --method GET*" = "allow";
-  "gh api --method=GET*" = "allow";
-  "gh api * --method GET*" = "allow";
-  "gh api * --method=GET*" = "allow";
-  "gh api -X HEAD*" = "allow";
-  "gh api -XHEAD*" = "allow";
-  "gh api * -X HEAD*" = "allow";
-  "gh api * -XHEAD*" = "allow";
-  "gh api --method HEAD*" = "allow";
-  "gh api --method=HEAD*" = "allow";
-  "gh api * --method HEAD*" = "allow";
-  "gh api * --method=HEAD*" = "allow";
-  "gh api -X OPTIONS*" = "allow";
-  "gh api -XOPTIONS*" = "allow";
-  "gh api * -X OPTIONS*" = "allow";
-  "gh api * -XOPTIONS*" = "allow";
-  "gh api --method OPTIONS*" = "allow";
-  "gh api --method=OPTIONS*" = "allow";
-  "gh api * --method OPTIONS*" = "allow";
-  "gh api * --method=OPTIONS*" = "allow";
-
-  "gh api -X get*" = "allow";
-  "gh api -Xget*" = "allow";
-  "gh api * -X get*" = "allow";
-  "gh api * -Xget*" = "allow";
-  "gh api --method get*" = "allow";
-  "gh api --method=get*" = "allow";
-  "gh api * --method get*" = "allow";
-  "gh api * --method=get*" = "allow";
-  "gh api -X head*" = "allow";
-  "gh api -Xhead*" = "allow";
-  "gh api * -X head*" = "allow";
-  "gh api * -Xhead*" = "allow";
-  "gh api --method head*" = "allow";
-  "gh api --method=head*" = "allow";
-  "gh api * --method head*" = "allow";
-  "gh api * --method=head*" = "allow";
-  "gh api -X options*" = "allow";
-  "gh api -Xoptions*" = "allow";
-  "gh api * -X options*" = "allow";
-  "gh api * -Xoptions*" = "allow";
-  "gh api --method options*" = "allow";
-  "gh api --method=options*" = "allow";
-  "gh api * --method options*" = "allow";
-  "gh api * --method=options*" = "allow";
-
-  "gh auth" = "deny";
-  "gh auth *" = "deny";
-
-  "gh config set" = "deny";
-  "gh config set *" = "deny";
-}
+let
+  rules = action: names:
+    builtins.listToAttrs (map (name: {
+        inherit name;
+        value = action;
+      })
+      names);
+  apiFields = builtins.concatMap (flag: ["gh api ${flag}*" "gh api * ${flag}*"]) ["-f" "-F" "--raw-field" "--field" "--input"];
+  apiMethods = builtins.concatMap (method: [
+    "gh api -X ${method}*"
+    "gh api -X${method}*"
+    "gh api * -X ${method}*"
+    "gh api * -X${method}*"
+    "gh api --method ${method}*"
+    "gh api --method=${method}*"
+    "gh api * --method ${method}*"
+    "gh api * --method=${method}*"
+  ]) ["POST" "PUT" "PATCH" "DELETE" "post" "put" "patch" "delete"];
+  cmds = builtins.concatLists (map ({
+    scope,
+    names,
+  }:
+    map (name: "gh ${scope} ${name}*") names) [
+    {
+      scope = "pr";
+      names = ["close" "comment" "create" "edit" "lock" "merge" "ready" "reopen" "revert" "review" "unlock" "update-branch"];
+    }
+    {
+      scope = "issue";
+      names = ["close" "comment" "create" "delete" "develop" "edit" "lock" "pin" "reopen" "transfer" "unlock" "unpin"];
+    }
+    {
+      scope = "repo";
+      names = ["archive" "create" "delete" "edit" "fork" "rename" "set-default" "sync" "unarchive"];
+    }
+    {
+      scope = "repo autolink";
+      names = ["create" "delete"];
+    }
+    {
+      scope = "repo deploy-key";
+      names = ["add" "delete"];
+    }
+    {
+      scope = "release";
+      names = ["create" "delete" "delete-asset" "edit" "upload"];
+    }
+    {
+      scope = "workflow";
+      names = ["disable" "enable" "run"];
+    }
+    {
+      scope = "cache";
+      names = ["delete"];
+    }
+    {
+      scope = "project";
+      names = ["close" "copy" "create" "delete" "edit" "field-create" "field-delete" "item-add" "item-archive" "item-create" "item-delete" "item-edit" "link" "mark-template" "unlink"];
+    }
+    {
+      scope = "label";
+      names = ["clone" "create" "delete" "edit"];
+    }
+    {
+      scope = "gist";
+      names = ["create" "delete" "edit" "rename"];
+    }
+    {
+      scope = "ssh-key";
+      names = ["add" "delete"];
+    }
+    {
+      scope = "gpg-key";
+      names = ["add" "delete"];
+    }
+    {
+      scope = "extension";
+      names = ["exec" "install" "remove" "upgrade"];
+    }
+    {
+      scope = "alias";
+      names = ["delete" "import" "set"];
+    }
+    {
+      scope = "secret";
+      names = ["delete" "set"];
+    }
+    {
+      scope = "variable";
+      names = ["delete" "set"];
+    }
+    {
+      scope = "agent-task";
+      names = ["create"];
+    }
+  ]);
+in
+  (rules "ask" (apiFields ++ apiMethods ++ cmds))
+  // (rules "deny" ["gh auth*" "gh config set*"])
